83acdea9233182c326c9bb7379aa9ac60fc6c51e
Cane Lagerwaard <cane@deity.io>
2018-11-15 14:33:22 +0100

    reminder to add tracking ID

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -95,6 +95,9 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   repoUrl: 'https://github.com/deity-io/falcon',
+  
+  // TODO: Add tracking ID
+  //gaTrackingId: '',
 };
 
 module.exports = siteConfig;
